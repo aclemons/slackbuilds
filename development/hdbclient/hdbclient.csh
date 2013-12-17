e26c42c1ad7e2c88e0f10f55d81d65e30d120ffe
Andrew Clemons <andrew.clemons@gmail.com>
2013-12-18 12:07:21 +1300

    tidy up profile variables for hdbclient

--- development/hdbclient/hdbclient.csh
+++
@@ -1,3 +0,0 @@
-#!/bin/csh
-setenv CLASSPATH "/usr/sap/hdbclient/ngdbc.jar:$CLASSPATH"
-setenv PATH ${PATH}:/usr/sap/hdbclient/
--- development/hdbclient/hdbclient.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export CLASSPATH="/usr/sap/hdbclient/ngdbc.jar:$CLASSPATH"
-export PATH="${PATH}:/usr/sap/hdbclient"
