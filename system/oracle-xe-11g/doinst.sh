bde11cab0695d0d3c46f9e4a34ddd7f0c58aa7e9
Andrew Clemons <andrew.clemons@gmail.com>
2014-02-12 15:53:35 +1300

    system/oracle-xe-11g: fix symlinks

--- system/oracle-xe-11g/doinst.sh
+++ system/oracle-xe-11g/doinst.sh
@@ -2,7 +2,6 @@ if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
 
-cat << EOF > $PKG/install/doinst.sh
 if ! grep /u01/app/oracle/product/11.2.0/xe/lib etc/ld.so.conf 1> /dev/null 2> /dev/null ; then
   echo "/u01/app/oracle/product/11.2.0/xe/lib" >> etc/ld.so.conf
 fi
@@ -10,6 +9,5 @@ fi
 if [ -x /sbin/ldconfig ]; then
   /sbin/ldconfig 2> /dev/null
 fi
-EOF
 
 # from oracle scripts
